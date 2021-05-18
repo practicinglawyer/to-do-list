d7cb8e347bd54eae3ff5006c316af7a012a27d79
practicinglawyer <79615260+practicinglawyer@users.noreply.github.com>
2021-05-18 16:43:30 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+double apples;
+double oranges;
+double cherries;
+double watermelons;
+double  wallet = 100;
+float userchoice;
+
+int main()
+{
+    cin.get();
+    return 0;
+}
