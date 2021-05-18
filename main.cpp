57e6e38785a40437ff504327eb9bc7ae0a3d8301
practicinglawyer <79615260+practicinglawyer@users.noreply.github.com>
2021-05-18 17:28:13 -0400

    adding cout

--- main.cpp
+++ main.cpp
@@ -11,6 +11,22 @@ float userchoice;
 
 int main()
 {
+
+    while (wallet >= 0)
+    {
+        cout << "\n\n welcome to T.J.'s!!\n\n";
+        cout << "Menu:\n\n";
+        cout << "1           Apples        $2\n";
+        cout << "2           Oranges       $3\n";
+        cout << "3           Cherries      $4\n";
+        cout << "2           watermelon    $5\n\n";
+        cout << "You have\n" << apples << "Apples\n" << Oranges << "Oranges\n" << Cherries << "Cherries\n" << watermelons << "watermelons \n\n";
+        cout << "Your available credit is $" << wallet <<".\n\n";
+    }
+    
+
+
+
     cin.get();
     return 0;
 }
